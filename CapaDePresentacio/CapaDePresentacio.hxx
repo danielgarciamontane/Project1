f3930b8dc27152ccb70bba6050d9ba56c6923412
dani <daniel.garcia.montane@estudiantat.upc.edu>
2025-12-28 20:41:32 +0100

    interfaz hecha

--- CapaDePresentacio/CapaDePresentacio.hxx
+++ CapaDePresentacio/CapaDePresentacio.hxx
@@ -22,6 +22,9 @@ private:
 	// Mètodes auxiliars
 	void mostrarMenuPrincipal();
 	void mostrarMenuSessioIniciada();
+	void mostrarMenuGestioUsuari();
+	void mostrarMenuGestioReserves();
+	void mostrarMenuConsultes();
 
 	string _usuariActual;
 	static CapaDePresentacio* _instancia;
